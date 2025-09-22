6f56569e80826e03cd80e906b5a4173c4439f0e1
Aryan Kesarwani <93967099+Aryankesarwani@users.noreply.github.com>
2025-09-22 11:44:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 //complete this code
-class Person {}
+class Person {
+	private name: string;
+	private age: number;
+	constructor(name: string, age: number){
+		this.name =  name;
+		this.age=age;
+	}
+	getName():string {
+		return this.name;
+	}
+	setName(name:string) void{
+		this.name=name;
+	}
+	getAge():number{
+		return this.age;
+	}
+	setAge(age:number)void{
+		this.age=age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	
+	study():void{
+		console.log(this.getName()+" is studying");
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+
+	teach():void{
+		console.log(this.getName+" is teaching");
+	}
+	
+}
 
 // Do not change the code below this line
 window.Person = Person;
