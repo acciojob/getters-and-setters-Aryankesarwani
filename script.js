a316297947674498cf72ce78fe3e285c5b1edecf
Aryan Kesarwani <93967099+Aryankesarwani@users.noreply.github.com>
2025-09-25 13:59:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -24,14 +24,15 @@ class Person {
 class Student extends Person {
 	
 	study(){
-		console.log(this.getName()+" is studying");
+		console.log(`${this.name} is studying`);
 	}
+	
 }
 
 class Teacher extends Person {
 
 	teach(){
-		console.log(this.getName+" is teaching");
+		console.log(`${this.name} is teaching`);
 	}
 	
 }
